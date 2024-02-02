833a0faf2811e04096ca477dedc1d3dcaed4fa12
Yeehawboizzz <s10258759@connect.np.edu.sg>
2024-02-02 17:29:42 +0800

    Updated random user code efficiency

--- JS/randomuser.js
+++ JS/randomuser.js
@@ -1,74 +1,19 @@
-let url = 'https://randomuser.me/api/';
+const url = 'https://randomuser.me/api/';
 
-function updateUser(){
+function updateUser(elementId) {
   fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username1").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username2").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username3").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username4").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username5").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username6").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username7").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username8").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username9").innerText = user.login.username;
-  });
-
-  fetch(url)
-  .then(response => response.json()) 
-  .then(function(data){
-    let user = data.results[0];
-    document.getElementById("username10").innerText = user.login.username;
-  });
+    .then(response => response.json())
+    .then(function (data) {
+      let user = data.results[0];
+      document.getElementById(elementId).innerText = user.login.username;
+    });
 }
 
+function updateAllUsers() {
+  for (let i = 1; i <= 10; i++) {
+    updateUser(`username${i}`);
+  }
+}
+
+// Call this function to update all users
+updateAllUsers();
